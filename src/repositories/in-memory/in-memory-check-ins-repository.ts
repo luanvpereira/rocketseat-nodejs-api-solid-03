1e147d0f21a3656b0e85b2362ad3c1c5028a4571
Luan Pereira <pereiralv2010@gmail.com>
2025-03-26 21:02:32 -0300

    feat: Fetch User Check-ins History Use Case

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -7,6 +7,12 @@ import dayjs from 'dayjs'
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public checkIns: CheckIn[] = []
 
+  async findManyByUserId(userId: string, page: number): Promise<CheckIn[]> {
+    return this.checkIns
+      .filter((checkIn) => checkIn.user_id === userId)
+      .slice((page - 1) * 20, page * 20)
+  }
+
   async findByUserIdOnDate(
     userId: string,
     date: Date,
